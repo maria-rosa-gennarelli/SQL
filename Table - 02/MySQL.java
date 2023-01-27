5d53b1092053ab34dc039774b660b2d0ba9ca70a
Maria Rosa <mariarosagennarelli@hotmail.com>
2023-01-27 19:38:17 +0100

    Edit exercise 02 - Table

--- Table - 02/MySQL.java
+++ Table - 02/MySQL.java
@@ -9,13 +9,13 @@ public class MySQL{
         try {
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/newdb", "root", "26199Mrg.");
             Statement statement = connection.createStatement();
-            //statement.executeUpdate("create table if not exists students(student_id int(10) auto_increment primary key not null)");
-            //statement.executeUpdate("alter table students add last_name varchar(30) not null after student_id");
-            //statement.executeUpdate("alter table students add first_name varchar(30) not null after last_name");
-            //statement.executeUpdate("insert into students (last_name, first_name) values ('Bocce', 'Mario')");
-            //statement.executeUpdate("insert into students (last_name, first_name) values ('Scacco', 'Aldo')");
-            //statement.executeUpdate("insert into students (last_name, first_name) values ('Felice', 'Sara')");
-            //statement.executeUpdate("insert into students (last_name, first_name) values ('Rossi', 'Andrea')");
+            statement.executeUpdate("create table if not exists student(student_id int(10) auto_increment primary key not null)");
+            statement.executeUpdate("alter table students add last_name varchar(30) not null after student_id");
+            statement.executeUpdate("alter table students add first_name varchar(30) not null after last_name");
+            statement.executeUpdate("insert into students (last_name, first_name) values ('Bocce', 'Mario')");
+            statement.executeUpdate("insert into students (last_name, first_name) values ('Scacco', 'Aldo')");
+            statement.executeUpdate("insert into students (last_name, first_name) values ('Felice', 'Sara')");
+            statement.executeUpdate("insert into students (last_name, first_name) values ('Rossi', 'Andrea')");
         } catch (Exception e) {
             System.out.println(e.getMessage());
 
